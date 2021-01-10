44735b9c461856581c757866dedf07b0461c0db2
tomdstanton <tomdstanton@gmail.com>
2021-01-10 18:45:50 +0000

    Postcode fixer

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from argparse import ArgumentParser
+from pandas import DataFrame
 from requests import get
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
 import urllib.parse
 
-parser = ArgumentParser(add_help=False, usage="", description="")
-parser.add_argument('postcode', default='EH93ED')
-parser.add_argument('query', default='')
+api_keys = {"trefle": "Fl172rMx2vXJDhRa6MYesqIvACkibM-kZ0JzOEtRPvA",
+            "plantnet": "2b10GOhutBEPgPOOZKAsVtygyO",
+            "owm": "b5608072b03d5196b9c9942fb2dc4cb0"}
 
-token = "Fl172rMx2vXJDhRa6MYesqIvACkibM-kZ0JzOEtRPvA"
-place = "?"
-species = "?"
-genus = "?"
-plants = "?"
+
+def fetch(url, site):
+    return get(url, headers={'Authorization': f'Bearer {api_keys[site]}'}).json()
+
+
 query = "daisy"
+postcode = 'EH93ED'
 
-header = "https://trefle.io/api/v1/"
-
-url = f'{header}plants/search?q={query}'
-r = get(url, headers={'Authorization': f'Bearer {token}'}).json()
-
+r = fetch(f'https://trefle.io/api/v1/plants/search?q={query}', "trefle")
 if len(r['data']) == 0:
-      print("No results found")
+    print("No results found")
 else:
-      df = pd.DataFrame(r['data'])
-      print(df[['common_name', 'slug']].to_markdown(index=False))
+    df: DataFrame = pd.DataFrame(r['data'])
+    print(df[['common_name', 'slug']].to_markdown(index=False))
 
-url = f'{header}plants/{df.slug[0]}'
-r = get(url, headers={'Authorization': f'Bearer {token}'}).json()
-
+r = fetch(f'https://trefle.io/api/v1/plants/{df.slug[0]}', "trefle")
 
 # Open Image from first result
 r = get(df.image_url[0], stream=True)
@@ -41,14 +36,13 @@ plt.imshow(image)
 plt.show()
 
 # Fetch latitude and longitude from postcode
+if ' ' not in postcode:
+      postcode = postcode[:-3] + ' ' + postcode[-3:]
 
-url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(postcode) +'?format=json'
+url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(postcode) + '?format=json'
 r = get(url).json()
 lat = r[0]["lat"]
 lon = r[0]["lon"]
 
 # Get weather for location
-owm_api_key = "b5608072b03d5196b9c9942fb2dc4cb0"
-url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={owm_api_key}'
-print(url)
-r = get(url).json()
+r = fetch(f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}', "owm")
