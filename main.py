dd307e05ee64b23def20bc77e86654a07c491c0a
tomdstanton <tomdstanton@gmail.com>
2021-01-10 18:19:08 +0000

    Location and weather searching

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
-import requests
+from requests import get
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
+import urllib.parse
 
-# if __name__ == "__main__":
-#     parser = ArgumentParser(add_help=False, usage="", description="")
-#
-#     parser.add_argument('input', nargs='*', default=['-'])
+parser = ArgumentParser(add_help=False, usage="", description="")
+parser.add_argument('postcode', default='EH93ED')
+parser.add_argument('query', default='')
 
 token = "Fl172rMx2vXJDhRa6MYesqIvACkibM-kZ0JzOEtRPvA"
 place = "?"
 species = "?"
 genus = "?"
 plants = "?"
-text_query = "tomato"
+query = "daisy"
 
-url = f'https://trefle.io/api/v1/' \
-      f'species/search?q={text_query}' \
-      f'&token={token}'
+header = "https://trefle.io/api/v1/"
 
-print(url)
+url = f'{header}plants/search?q={query}'
+r = get(url, headers={'Authorization': f'Bearer {token}'}).json()
 
-r = requests.get(url).json()
+if len(r['data']) == 0:
+      print("No results found")
+else:
+      df = pd.DataFrame(r['data'])
+      print(df[['common_name', 'slug']].to_markdown(index=False))
 
-df = pd.DataFrame(r['data'])
+url = f'{header}plants/{df.slug[0]}'
+r = get(url, headers={'Authorization': f'Bearer {token}'}).json()
 
-response = requests.get(df.image_url[0], stream=True)
-response.raw.decode_content = True
-image = Image.open(response.raw)
+
+# Open Image from first result
+r = get(df.image_url[0], stream=True)
+r.raw.decode_content = True
+image = Image.open(r.raw)
 plt.imshow(image)
 plt.show()
+
+# Fetch latitude and longitude from postcode
+
+url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(postcode) +'?format=json'
+r = get(url).json()
+lat = r[0]["lat"]
+lon = r[0]["lon"]
+
+# Get weather for location
+owm_api_key = "b5608072b03d5196b9c9942fb2dc4cb0"
+url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={owm_api_key}'
+print(url)
+r = get(url).json()
